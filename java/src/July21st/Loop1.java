2adca9eec9abe7b779186f3d23946b3f5215a7c5
sahilhaider962002 <sahilhaider932@gmail.com>
2024-07-21 23:30:21 +0530

    First Push

---
+++ java/src/July21st/Loop1.java
@@ -0,0 +1,14 @@
+package July21st;
+
+public class Loop1 {
+    public static void main(String[] args) {
+        int n=3;
+        while(n%2==0){
+            System.out.println("true");
+
+        }
+        System.out.println("False");
+
+    }
+
+}
