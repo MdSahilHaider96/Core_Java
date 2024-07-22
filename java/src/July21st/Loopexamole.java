d32c5a67ca046953a09b23ce9cc77f994f7bca6c
sahilhaider962002 <sahilhaider932@gmail.com>
2024-07-22 15:40:35 +0530

    push

--- java/src/July21st/Loopexamole.java
+++ java/src/July21st/Loopexamole.java
@@ -3,7 +3,7 @@ package July21st;
 public class Loopexamole {
     public static void main(String[] args) {
         int n=3;
-        while(n%2==0){
+         while(n%2==0);{
             System.out.println("true");
 
         }
