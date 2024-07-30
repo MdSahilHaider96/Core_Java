906d49269382d2f3e27f8cf8842089392ec947f0
sahilhaider962002 <sahilhaider932@gmail.com>
2024-07-30 20:19:13 +0530

    push

--- java/src/July30th/July25th/Array3.java
+++
@@ -1,9 +0,0 @@
-package July30th.July25th;
-
-public class Array3 {
-    public static void main(String[] args) {
-        //03) 	wap to find length of an array
-        int[] a={9,3,4,5,2,3};
-        System.out.println(a.length);
-    }
-}
