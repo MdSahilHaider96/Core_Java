f0459353240bebca52a0e65c921c9bbd5bec948a
Sahil <mdsahilhaider96@gmail.com>
2024-10-06 01:58:00 +0530

    5thOctober

--- java/src/September25th/BirdsMain.java
+++ java/src/September25th/BirdsMain.java
@@ -1,7 +1,7 @@
 package September25th;
 
 public class BirdsMain {
-    public static void main(String[] args) {
+    public static void main() {
         Birds birds = new Birds();
         birds.fly();
         birds.fly1();
---
+++ java/src/September25th/SortedArray.java
@@ -0,0 +1,13 @@
+package September25th;
+
+import java.util.Arrays;
+
+public class SortedArray {
+    public static void main(String[] args) {
+        int[] arr={5,26,14,22,5};
+         Arrays.sort(arr);
+         for (int x: arr){
+             System.out.println(x);
+         }
+    }
+}
